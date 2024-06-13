df5a9d1dc5f9d45af1585d449c52c095be91ddff
Carlos <carloscontreras9522@gmail.com>
2024-06-13 13:27:08 +0200

    Creamos la clase provider y hemos movido la lista de horoscopo a dentro de la misma. De esta manera no necesitamos iniciar la aplicación desde el MainActiviy, sino despues cualquier sitio

--- app/src/main/java/com/example/horoscope/DetailActivity.kt
+++ app/src/main/java/com/example/horoscope/DetailActivity.kt
@@ -6,16 +6,30 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_HOROSCOPE_ID = "HOROSCOPE_ID"
+    }
+
+    lateinit var horoscope: Horoscope
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val id = intent.getStringExtra("HOROSCOPE_ID")
+        val id = intent.getStringExtra(EXTRA_HOROSCOPE_ID)
 
-        val name = intent.getIntExtra("HOROSCOPE_NAME", -1)
-        val logo = intent.getIntExtra("HOROSCOPE_LOGO", -1)
+        //Comentamos las dos variables para añadir a la funcion onCreate,
+        //el HoroscopeProvider y los busque por sus respectivos ID.
+        //val name = intent.getIntExtra("HOROSCOPE_NAME", -1)
+        //val logo = intent.getIntExtra("HOROSCOPE_LOGO", -1)
+        horoscope = HoroscopeProvider.findById(id!!)!!
 
-        findViewById<TextView>(R.id.textView).setText(name)
-        findViewById<ImageView>(R.id.imageView).setImageResource(logo)
+        //Volvemos a comentar estos dos finView porque ahora mostraremos,
+        //el texto y la imagen a través de la llamada en el DetailActivity
+        //findViewById<TextView>(R.id.textView).setText(name)
+        //findViewById<ImageView>(R.id.imageView).setImageResource(logo)
+        findViewById<TextView>(R.id.textView).setText(horoscope.name)
+        findViewById<ImageView>(R.id.imageView).setImageResource(horoscope.logo)
     }
 }
